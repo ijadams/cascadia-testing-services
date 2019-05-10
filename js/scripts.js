338dd05904ab5d2f97b522434964585439e4f2e5
Ian Adams <ian.adams@ge.com>
2019-05-10 11:18:30 -0500

    video mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 // Javascript goes here
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // console.log('mobile');
+} else {
+    var sources = document.querySelectorAll('video source');
+    // Define the video object this source is contained inside
+    var video = document.querySelector('video');
+    for(var i = 0; i<sources.length;i++) {
+      sources[i].setAttribute('src', sources[i].getAttribute('data-src'));
+    }
+    // If for some reason we do want to load the video after, for desktop as opposed to mobile (I'd imagine), use videojs API to load
+    video.load();
+    video.muted= "muted";
+}
